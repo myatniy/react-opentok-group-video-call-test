3fae31306be72a4b7c25bdefbc0c6fe753ee0bb3
myatniy <ipsixin3d@gmail.com>
2021-01-07 01:09:50 +0300

    Test opentok's react sample app.

---
+++ src/config.js
@@ -0,0 +1,21 @@
+// Make a copy of this file and save it as config.js (in the src directory).
+
+// Set this to the base URL of your sample server, such as 'https://your-app-name.herokuapp.com'.
+// Do not include the trailing slash. See the README for more information:
+
+const SAMPLE_SERVER_BASE_URL = 'http://YOUR-SERVER-URL';
+
+// OR, if you have not set up a web server that runs the learning-opentok-php code,
+// set these values to OpenTok API key, a valid session ID, and a token for the session.
+// For test purposes, you can obtain these from https://tokbox.com/account.
+
+const API_KEY = '47058744';
+const SESSION_ID = '2_MX40NzA1ODc0NH5-MTYwOTY4ODQ4NzYxM35EQU1maWlDaW5BcFV2TlBRMDA3dGVKbDl-fg';
+const TOKEN = 'T1==cGFydG5lcl9pZD00NzA1ODc0NCZzaWc9OGNkMjEyYTYzODI4OWQwODc2ZWZlYTE0MzBkM2YzNmUyNGUwZTU1OTpzZXNzaW9uX2lkPTJfTVg0ME56QTFPRGMwTkg1LU1UWXdPVFk0T0RRNE56WXhNMzVFUVUxbWFXbERhVzVCY0ZWMlRsQlJNREEzZEdWS2JEbC1mZyZjcmVhdGVfdGltZT0xNjA5Njg4NTA5Jm5vbmNlPTAuNjI1MDI5ODk1NTM2NTExMiZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjEyMjgwNTA4JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9';
+
+export {
+  SAMPLE_SERVER_BASE_URL,
+  API_KEY,
+  SESSION_ID,
+  TOKEN,
+};
